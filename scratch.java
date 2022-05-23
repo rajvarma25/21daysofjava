a6b4a30039c526a030081c7ef9ac84a04ac46bce
Raj <65528610+rajvarma25@users.noreply.github.com>
2022-05-23 23:14:23 +0530

    Add files via upload

---
+++ scratch.java
@@ -0,0 +1,26 @@
+class Scratch {
+    public static void main(String[] args) {
+        String name ="Raj";
+        System.out.println(name);
+    }
+}
+class prog2 {
+    public static void main(String[] args) {
+        int num = 88;
+        System.out.println(num);
+    }
+}
+
+class prog3 {
+    public static void main(String[] args) {
+        int num = 9;
+        num = 25;
+        System.out.println(num);
+    }
+}
+
+class prog4{
+    public static void main(String[] args) {
+
+    }
+}
